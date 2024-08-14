6e13362f527f7efd33ea23ea95a0e822daba1279
KristaaP <krista41319@gmail.com>
2024-08-14 13:40:54 -0400

    Added github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was codede by Krista Phillips and is{" "}
+        <a href="https://github.com/KristaaP/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
